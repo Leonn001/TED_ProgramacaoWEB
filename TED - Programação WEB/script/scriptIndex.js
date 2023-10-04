a261991aa0e1240e0500c1231b550fc71225085f
Leon Nascimento Moreira <126993735+Leonn001@users.noreply.github.com>
2023-10-04 08:47:15 -0300

    Delete TED - Programação WEB directory

--- TED - Programação WEB/script/scriptIndex.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('prosseguirButton').addEventListener('click', function() {
-    const nome = document.getElementById('nomeInput').value;
-    localStorage.setItem('nome', nome);
-    window.location.href = 'menu.html';
-});
